4eaaae532a9e4013b530b599c0e2747197c96622
vishnumohan7 <63349875+vishnumohan7@users.noreply.github.com>
2022-12-14 19:39:36 +0530

    task

--- lib/src/models/task_model.dart
+++ lib/src/models/task_model.dart
@@ -2,21 +2,15 @@ class TaskModel{
 
   String title;
   String description;
-  DateTime endDate;
-  DateTime startDate;
-  bool isCompleted;
+
   String? id;
 
-  TaskModel({required this.title,required this.description,required this.endDate,required this.startDate,
-     required this.isCompleted, this.id});
+  TaskModel({required this.title,required this.description, this.id});
 
   factory TaskModel.fromJson(Map<String, dynamic> json) {
     return TaskModel(
       title: json["title"],
       description: json["description"],
-      endDate: DateTime.parse(json["endDate"]),
-      startDate: DateTime.parse(json["startDate"]),
-      isCompleted: json["isCompleted"],
       id: json["id"]
     );
   }
@@ -25,9 +19,6 @@ class TaskModel{
     return {
       "title": this.title,
       "description": this.description,
-      "endDate": this.endDate.toIso8601String(),
-      "startDate": this.startDate.toIso8601String(),
-      "isCompleted": this.isCompleted,
       "id": this.id
     };
   }
